52cbb15031b72bf962f7ae6d0c82c2ac71027d8b
Venkata Sai Mounish Ennamuri <69751378+mounishennamuri888@users.noreply.github.com>
2023-10-29 15:09:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const bill_t= document.querySelector("#bill-total");
-const slider = document.querySelector("#tip");
 const tip_percentage = document.querySelector("#tip-percentage");
 const tip_amount = document.querySelector("#tip-amount");
 const total_amount = document.querySelector("#total");
+const bill_t= document.querySelector("#bill-total");
+const slider = document.querySelector("#tip");
 
 bill_t.addEventListener("change", calculateTip);
 slider.addEventListener("input", calculateTip);
 
+//Defining a function to calculate Tip
 function calculateTip() {
     if (isNaN(bill_t.value)) {
         alert("ENTER AN INPUT VALUE WHICH IS VALID")
     }
+    
     bill_t.value = parseFloat(bill_t.value).toFixed(2);
     let bill = parseFloat(document.getElementById("bill-total").value);
     let tip = document.getElementById("tip").value;
 
     tip_percentage.value = tip;
-
     let total_tip = parseFloat(((tip * bill) / 100).toFixed(2));
 
     tip_amount.value = total_tip;
-
+    // total amount including tip
     total_amount.value = parseFloat(bill + total_tip).toFixed(2);
 }
